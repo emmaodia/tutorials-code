efd4d120612fa10804214b0b046875850592922e
emmaodia <odia.emma@gmail.com>
2022-06-20 23:43:06 +0100

    NEAR Donor Contract.

---
+++ near-donor/target/debug/deps/near_employee_log-542952fa5052f509.d
@@ -0,0 +1,5 @@
+/Users/emmanuel/Projects/tutorials-code/near-employee-log/target/debug/deps/near_employee_log-542952fa5052f509.rmeta: src/lib.rs
+
+/Users/emmanuel/Projects/tutorials-code/near-employee-log/target/debug/deps/near_employee_log-542952fa5052f509.d: src/lib.rs
+
+src/lib.rs:
---
+++ near-donor/target/debug/deps/near_employee_log-8c096fbc8940bc6b.d
@@ -0,0 +1,5 @@
+/Users/emmanuel/Projects/tutorials-code/near-employee-log/target/debug/deps/near_employee_log-8c096fbc8940bc6b.rmeta: src/lib.rs
+
+/Users/emmanuel/Projects/tutorials-code/near-employee-log/target/debug/deps/near_employee_log-8c096fbc8940bc6b.d: src/lib.rs
+
+src/lib.rs:
