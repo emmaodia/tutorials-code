4802e859be98097300c4a966929077cad23a14be
Emmanuel Oaikhenan <odia.emma@gmail.com>
2024-11-24 21:27:19 +0100

    Forms.

--- onchain-employee-logbook/app/page.tsx
+++ onchain-employee-logbook/app/page.tsx
@@ -8,6 +8,32 @@ export default function Home() {
       <button className="px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Sign Out
       </button>
+      <div className="mt-10">
+        <form className="flex max-w-md flex-col gap-4">
+          Get Attendance Record
+          <div className="mt-3">
+            <label
+              htmlFor="amount-input"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Employee Address
+            </label>
+            <input
+              type="text"
+              id="amount-input"
+              placeholder="Enter Employee Address"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
